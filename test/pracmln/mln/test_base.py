3f28f3da3e5f43da2b130ab884912a10f22fe6d2
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-09-20 17:02:54 +0200

    test(mln): Adds multiple mln files for testing

--- test/pracmln/mln/test_base.py
+++ test/pracmln/mln/test_base.py
@@ -1,7 +1,25 @@
+from pracmln.mln import MLNParsingError
 from pracmln.mln.base import parse_mln
 import os.path as path
+from os import listdir
+
+TEST_MLNS_FOLDER_PATH = path.join(path.dirname(__file__),"test_mlns")
 
 
 def test_performance_parse_mln():
-    with open(path.join(path.dirname(__file__),"performance_test.mln"), "r") as mln_file:
-        parse_mln(mln_file.read())
+    num_wrong_parsed_mlns = 0
+
+    for mln_filename in listdir(TEST_MLNS_FOLDER_PATH):
+        if mln_filename == 'performance_test.mln': continue
+
+        mln_file_path = path.join(TEST_MLNS_FOLDER_PATH, mln_filename)
+        with open(mln_file_path, "r") as mln_file:
+
+            try:
+                parse_mln(mln_file.read())
+            except MLNParsingError as e:
+                print("Parsing Error in: " + mln_file_path)
+                print(e)
+                num_wrong_parsed_mlns += 1
+
+    assert num_wrong_parsed_mlns == 0
