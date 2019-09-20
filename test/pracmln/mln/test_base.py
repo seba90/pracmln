5620a85e03bcf30110d3c685d9b1305e64772052
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-09-20 16:13:43 +0200

    test(mln): Adds mln parsing performance test

--- test/pracmln/mln/test_base.py
+++ test/pracmln/mln/test_base.py
@@ -1,5 +1,7 @@
 from pracmln.mln.base import parse_mln
+import os.path as path
 
 
-def test_parse_mln():
-    assert 0 == 0
+def test_performance_parse_mln():
+    with open(path.join(path.dirname(__file__),"performance_test.mln"), "r") as mln_file:
+        parse_mln(mln_file.read())
