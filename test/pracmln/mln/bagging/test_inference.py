c3ec647e64ddcd4b684f3b6af41a12b9da52929b
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-10-17 14:51:24 +0200

    test(bagging): Update test to ensemble mln class

--- test/pracmln/mln/bagging/test_inference.py
+++ test/pracmln/mln/bagging/test_inference.py
@@ -2,26 +2,22 @@ from os import path
 
 from pracmln.mln.base import parse_mln
 from scipy import sparse
+from pracmln.mln.database import Database
+import numpy as np
+
+from python3.pracmln.mln.bagging.ensemble_mln import EnsembleMLN
 
 
 def test_inference():
     mln_file = open(path.join(path.dirname(__file__), 'bagging.mln'))
     mln = parse_mln(mln_file.read())
     materialized_mln = mln.materialize()
+    mrf = materialized_mln.ground(Database(materialized_mln))
     mln_file.close()
 
-    val = []
-    col_ind = []
-    row_ptr = [0]
+    ensemble = EnsembleMLN(mln, Database(materialized_mln))
+    ensemble.perform_exact_inference_with_given_world(list(mrf.worlds())[1])
 
-    for formula in materialized_mln.formulas:
-        sparse_vector = eval(formula.weight)
-        vector_index, vector_value = sparse_vector
-        val.extend(vector_value)
-        col_ind.extend(vector_index)
-        row_ptr.append(row_ptr[-1]+len(vector_value))
-
-    sparse_weight_matrix = sparse.csr_matrix((val,col_ind,row_ptr))
     assert 0 == 0
 
 
